19fae588f85e316374c40b8c2dceeda0c6b7adfa
swathi.m251297@gmail.com <swathi.m251297@gmail.com>
2019-08-02 10:49:37 +0530

    backend

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
+var helmet = require('helmet');
+var compression = require('compression');
+var csrf = require('csurf');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+dotenv.config();
+
+// database connection
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true},(err,result)=>{
+  if(err) return console.log("Error Occured========>",err);
+  console.log("Database Connected");
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(compression());
+app.use(csrf({ cookie: true }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "express-api",
+  "version": "0.0.0",
+  "description": "apiDoc for express server",
+  "apidoc": "0.3.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-08-01T08:01:35.764Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
